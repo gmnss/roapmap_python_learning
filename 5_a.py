57397103a45bbce587e7185445fef07c01833f3e
gmnss <gmenesse@gmail.com>
2023-02-12 14:17:03 -0300

    add exercises 5

---
+++ 5_a.py
@@ -0,0 +1,20 @@
+false = False
+true = True
+
+
+list_example = [1,2,3,4,5]
+print(list_example)
+list_example_two = [1,'hola',True,true,654.654, {}]
+print(list_example_two)
+
+tuple_example = (1,2,3,4,5,6,7,'hola', {1.123}, ())
+print(tuple_example)
+
+set_example = {True, 3.14, None, False, "Hola mundo", (1, 2)}
+print(set_example)
+
+dict_example = {
+  'name': 'Guillermo',
+  'carrers': 'developer Python'
+}
+print(dict_example)
