807ae90984f72cefe12f813809e009b71be06281
gmnss <gmenesse@gmail.com>
2023-02-12 12:51:49 -0300

    add exercises 3

--- 2_a.py
+++ 2_a.py
@@ -9,3 +9,7 @@ print(valor1 // 5) # 20
 
 # Cálculo de Potencias
 print(valor1 ** valor2) # 10000000000
+
+# notación cientifica
+print(10e2) # 1000.0
+print(10e3) # 10000.0
---
+++ 3_a.py
@@ -0,0 +1,67 @@
+resultado_n = ((1000 % 2) ** 10) / (100 * 5)
+print(1000 % 2)
+print((1000 % 2) ** 10)
+print(100 * 5)
+print(resultado_n)
+
+hi = "hi!"
+print(hi)
+hi += " world"
+print(hi)
+#hi -= " world"
+print(hi)
+
+number_one = 1
+number_two = 1
+
+total = number_one + number_two
+print(total)
+total += number_one
+print(total)
+total -= -10
+print(total)
+total *= total
+print(total)
+
+"""
+=   Asigna valores del lado derecho a la variable del lado izquierdo.
++=  Agrega el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+-=  Sustrae el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+/=  Divide el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+//= Realiza la división de piso entre los 2 valores (número entero) y asigna valor a la variable izquierda.
+%=  Entrega el módulo usando los 2 valores y asigna el resultado a la variable izquierda.
+*=  Multiplica el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+**= Realiza cálculos exponenciales (potencia) entre los valores y asigna valor a la variable izquierda.
+"""
+
+# =   Asigna valores del lado derecho a la variable del lado izquierdo.
+variable1 = 100
+print(variable1)
+
+# +=  Agrega el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+variable1 += 1
+print(variable1)
+
+# -=  Sustrae el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+variable1 -= 1
+print(variable1)
+
+# /=  Divide el valor derecho a la variable izquierda y asigna el resultado a la variable izquierda.
+variable1 /= 2
+print(variable1)
+
+# //= Realiza la división de piso entre los 2 valores (número entero) y asigna valor a la variable izquierda.
+variable1 //= 2.3
+print(variable1)
+
+# %=  Entrega el módulo usando los 2 valores y asigna el resultado a la variable izquierda.
+variable1 %= 6
+print(variable1)
+
+# *=  Entrega el módulo usando los 2 valores y asigna el resultado a la variable izquierda.
+variable1 *= 2
+print(variable1)
+
+# **= Realiza cálculos exponenciales (potencia) entre los valores y asigna valor a la variable izquierda.
+variable1 **= 5
+print(variable1)
