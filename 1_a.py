44ec29a57ea870d9742c429e0a4378c8e90bfa29
gmnss <gmenesse@gmail.com>
2023-02-12 11:44:45 -0300

    add exercises 1

---
+++ 1_a.py
@@ -0,0 +1,16 @@
+hola = 'hola' # caracteres
+numero = '123123123' # caracteres
+numero_int = 654654321 # entero
+numero_float = 132.456 # float
+
+precio_comida = 10_000_000
+
+print(precio_comida + precio_comida)
+print(precio_comida + numero_float)
+print(precio_comida / numero_float)
+print(precio_comida * numero_float)
+
+# crear repositorio para las actividades de python (roapmap_python_learning)
+# que resultado nos da hola + numero
+print(hola + numero) # hola123123123
+# desmotremos que sabemos matematicas con python
