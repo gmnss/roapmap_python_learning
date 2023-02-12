b5dca93105bd665283ddd042d975f8743c2704a0
gmnss <gmenesse@gmail.com>
2023-02-12 11:59:22 -0300

    add exercises 2

---
+++ 2_a.py
@@ -0,0 +1,11 @@
+valor1 = 100
+valor2 = 5
+
+# Cálculo de Resto (o resisuo)
+print(valor1 % valor2) # 0
+
+# Cálculo de división con resultado de numero entero
+print(valor1 // 5) # 20
+
+# Cálculo de Potencias
+print(valor1 ** valor2) # 10000000000
